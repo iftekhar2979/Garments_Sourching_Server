0acc11ed79fd36ea0184aaf26d17717b86606a48
Iftekhar Salmin Rashid <97031620+iftekhar2979@users.noreply.github.com>
2024-02-04 00:30:49 +0600

    Update connect.js

--- Database/connect.js
+++ Database/connect.js
@@ -1,10 +1,10 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 // node js 5.5 or later
-// const uri=`mongodb+srv://abcSourching:74hfL8AVGih9VVcp@cluster0.0iunmqq.mongodb.net/?retryWrites=true&w=majority`
+
 const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0iunmqq.mongodb.net/?retryWrites=true&w=majority`
 
-// const localUri='mongodb://127.0.0.1:27017'
+
 mongoose.connect(uri,{
     useUnifiedTopology: true,
     useNewUrlParser:true
